c6b0d6c4ca1a5324fd764a603a142b3eee868f16
SupasanRuang <supasan_ruang@cmu.ac.th>
2021-11-26 15:50:29 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    int num,have_odd=0,have_even=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin>>num;
+    while (num!=0)
+    {
+        if(num%2==1)
+        {
+            have_odd++;
+
+        }
+       else 
+        {
+            have_even++;
+
+        }
+        cout << "Enter an integer: ";
+        cin>>num;
+    }
+    
+    cout << "#Even numbers = "<<have_even<<endl;
+    cout << "#Odd numbers = "<<have_odd<<endl;
     return 0;
 }
